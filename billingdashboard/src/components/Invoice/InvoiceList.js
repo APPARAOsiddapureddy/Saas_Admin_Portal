e26034a93243bc386a83810417893ac50d6b914d
APPARAOsiddapureddy <122093922+APPARAOsiddapureddy@users.noreply.github.com>
2025-05-08 12:45:26 +0530

    Update InvoiceList.js

--- billingdashboard/src/components/Invoice/InvoiceList.js
+++ billingdashboard/src/components/Invoice/InvoiceList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useInvoices } from '../../hooks/UseInvoices';
+import { useInvoices } from '../../hooks/useInvoices';
 
 const InvoicesList = () => {
   const { invoices, deleteInvoice } = useInvoices();
